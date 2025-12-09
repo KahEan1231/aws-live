daf9548308c2b0b726952e312abe8e240c265aa1
KahEan1231 <112403151+KahEan1231@users.noreply.github.com>
2025-12-09 10:32:40 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cuqursqpvr6y.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "wongkahean-employee"
+customregion = "us-east-1"
+
